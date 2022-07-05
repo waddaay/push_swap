51f0612fc75fc1a3e25a63d4ab2cd2f332af6b17
Yassine Wadday <ywadday@e3r9p10.1337.ma>
2022-07-05 22:52:15 +0100

    sorted stack function need to be fixed

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ywadday <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 01:51:45 by ywadday           #+#    #+#             */
-/*   Updated: 2022/07/05 05:33:19 by ywadday          ###   ########.fr       */
+/*   Updated: 2022/07/05 22:49:03 by ywadday          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -131,4 +131,55 @@ void    push(int *stack_x, int *stack_y, int size_x, int size_y)
     if (size_y == 1) return ;
     rotate(stack_y, size_y);
     stack_y[--size_y] = 0;
+}
+
+int    *sorted_stack(int *stack_a, int size)
+{
+    int *stack_s;
+    int i;
+    int j;
+    int tmp;
+    
+    stack_s = create_stack(size);
+    i = 0;
+    while (i++ < size)
+        push(stack_s, stack_a, size, size);
+    i = 0;
+    while (i < size)
+    {
+        j += i;
+        while (j < size - i)
+        {
+            if (stack_s[i] > stack_s[j])
+            {
+               tmp = stack_s[i];
+                stack_s[i] = stack_s[j];
+                stack_s[j] = tmp;
+            }
+            j++;
+        }
+        i++;
+    }
+    return (stack_s);   
+}
+
+int main(int argc, char **argv)
+{
+    int size = stack_size(argc, argv);
+    int *stack_a, *stack_b, *stack_s;
+    int i = 0;
+
+    stack_a = create_stack(size);
+    stack_b = create_stack(size);
+
+    stack_b[0] = -9;
+    stack_b[1] = 1;
+    stack_b[2] = 2;
+    stack_b[3] = 7;
+    
+    fill_stack(argc, argv, stack_a, size);
+    push(stack_a,stack_b, size, size);
+    stack_s = sorted_stack(stack_a, size);
+    while (i < size)
+        printf("%d\n", stack_s[i++]);
 }
