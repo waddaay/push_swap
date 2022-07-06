ad31ce7cc8cc27b987935a42a9b090ab438a19d6
Yassine Wadday <ywadday@e1r2p13.1337.ma>
2022-07-06 22:07:46 +0100

    fixed

--- sort.c
+++ sort.c
@@ -6,38 +6,36 @@
 /*   By: ywadday <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 22:54:17 by ywadday           #+#    #+#             */
-/*   Updated: 2022/07/05 22:54:35 by ywadday          ###   ########.fr       */
+/*   Updated: 2022/07/06 19:22:20 by ywadday          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int    *sorted_stack(int *stack_a, int size)
+t_stack    *sorted_stack(t_stack *stack_a)
 {
-    int *stack_s;
-    int i;
-    int j;
-    int tmp;
+    t_stack *stack_s;
+    int     tmp;
+    int     i;
+    int     j;
     
-    stack_s = create_stack(size);
+    stack_s = create_stack(stack_a->size);
+    ft_memmove(stack_s, stack_a, stack_a->size*4);
     i = 0;
-    while (i++ < size)
-        push(stack_s, stack_a, size, size);
-    i = 0;
-    while (i < size)
+    while (i < stack_s->size)
     {
-        j += i;
-        while (j < size - i)
+        j = i + 1;
+        while (j < stack_s->size)
         {
-            if (stack_s[i] > stack_s[j])
+            if (stack_s->body[i] < stack_s->body[j])
             {
-               tmp = stack_s[i];
-                stack_s[i] = stack_s[j];
-                stack_s[j] = tmp;
+                tmp = stack_s->body[i];
+                stack_s->body[i] = stack_s->body[j];
+                stack_s->body[j] = tmp;
             }
             j++;
         }
         i++;
     }
-    return (stack_s);   
+    return stack_s;
 }
