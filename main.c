5f661d75b3549a6dedde4fa36b936da490d76b33
Yassine Wadday <ywadday@e1r5p1.1337.ma>
2022-07-09 03:07:38 +0100

    maaain

--- main.c
+++ main.c
@@ -6,30 +6,49 @@
 /*   By: ywadday <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 01:51:45 by ywadday           #+#    #+#             */
-/*   Updated: 2022/07/05 22:57:00 by ywadday          ###   ########.fr       */
+/*   Updated: 2022/07/09 03:03:18 by ywadday          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-
 int main(int argc, char **argv)
 {
     int size = stack_size(argc, argv);
-    int *stack_a, *stack_b, *stack_s;
-    int i = 0;
+    t_stack *stack_a, *stack_b, *stack_s;
 
-    stack_a = create_stack(size);
-    stack_b = create_stack(size);
+    stack_a = create_stack(size, 0);
+    stack_b = create_stack(size, size);
+    fill_stack(argc, argv,stack_a);
+    stack_s = sorted_stack(stack_a);
+    // printf("%d", stack_a->body[0]);
+    // for (int i = 0; i < stack_a->size; i++)
+    //     printf("%d\n", stack_a->body[i]);
+    // for (int i = stack_s->top; i < stack_s->size; i++)
+    //     printf("%d, ", stack_s->body[i]);
+    push_all_to_b(stack_a, stack_b, stack_s, 3);
+    // for (int i = 0; i < stack_a->size; i++)
+    //     pb(stack_a, stack_b);
+    // for (int i = 0; i < stack_b->size; i++)
+    //     printf("%d\n", stack_b->body[i]);
 
-    stack_b[0] = -9;
-    stack_b[1] = 1;
-    stack_b[2] = 2;
-    stack_b[3] = 7;
+    // printf("\n---------------------------\n");
+    // for (int i = 0; i < stack_b->size; i++)
+    //     pa(stack_a, stack_b);
+    for (int i = stack_a->top; i < stack_a->size; i++)
+        printf("%d, ", stack_a->body[i]);
+
+    printf("\n---------------------------\n");
     
-    fill_stack(argc, argv, stack_a, size);
-    push(stack_a,stack_b, size, size);
-    stack_s = sorted_stack(stack_a, size);
-    while (i < size)
-        printf("%d\n", stack_s[i++]);
+    // for (int i = 0; i < stack_a->size; i++)
+    //     rrotate(stack_a, "rra\n");
+    // for (int i = 0; i < stack_a->size; i++)
+    //     printf("%d\n", stack_a->body[i]);
+    for (int i = stack_b->top; i < stack_b->size; i++)
+        printf("%d, ", stack_b->body[i]);
+    printf("\n\n%d", max(stack_b));
+    printf("\n\n%d", max(stack_a));
+    printf("\n\n%d", max(stack_s));
+    
+    
 }
