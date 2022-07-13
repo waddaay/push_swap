9e9e30afcc386786814f694923df9503821a3d4a
Yassine Wadday <ywadday@e1r10p12.1337.ma>
2022-07-13 16:23:56 +0100

    dosen't work

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: ywadday <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 22:54:17 by ywadday           #+#    #+#             */
-/*   Updated: 2022/07/09 05:52:50 by ywadday          ###   ########.fr       */
+/*   Updated: 2022/07/13 16:23:35 by ywadday          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,16 +54,11 @@ void push_all_to_b(t_stack *stack_a, t_stack *stack_b, t_stack *stack_s, int div
     pv1 = stack_s->size / div;
     while (pv1 < stack_a->size - 1)
     {
-        // pv1 = (stack_s->size + stack_s->top) / div;
-        pv1 *= div;
-        if (pv1 >= stack_a->size)
-            pv1 = stack_a->size - 1;
         stack_s->top = stack_s->top + pv1;
         pv2 = pv1 / 2;
         i = 0;
-        while (i++ < stack_s->top && stack_a->top != stack_a->size - 1)
+        while (i++ < stack_s->size - stack_s->top)
         {
-            
             if (stack_a->body[stack_a->top] < stack_s->body[pv1])
             {
                 pb(stack_a, stack_b);
@@ -76,6 +71,7 @@ void push_all_to_b(t_stack *stack_a, t_stack *stack_b, t_stack *stack_s, int div
             else
                 rotate(stack_a, "ra\n");
         }
+        pv1 = (stack_s->size + stack_s->top ) / div;
     }
     free(stack_s->body);
     free(stack_b);
@@ -128,7 +124,6 @@ void push_all_to_a(t_stack *stack_a, t_stack *stack_b)
                 rrotate(stack_b, "rrb\n");
                 max_indice++;
                 printf("%d %d\n", max_indice, stack_b->size);
-                // sleep(1);
             }
         }
         pa(stack_a, stack_b);
