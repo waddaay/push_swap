bec5b8855af61817f9147b884fdf399c6f284cfe
Yassine Wadday <ywadday@e2r1p5.1337.ma>
2022-07-14 23:36:07 +0100

    I didn't used those functions

---
+++ swap.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ywadday <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 22:09:22 by ywadday           #+#    #+#             */
+/*   Updated: 2022/07/06 22:25:39 by ywadday          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void    swap(t_stack *stack)
+{
+    int tmp;
+
+    tmp = stack->body[stack->top];
+    stack->body[stack->top] = stack->body[stack->top + 1];
+    stack->body[stack->top + 1] = tmp;
+}
+
+void sa(t_stack *stack_a)
+{
+    if (stack_a->top == stack_a->size)
+        return ;
+    swap(stack_a);
+    ft_putstr_fd("sa\n", 1);
+}
+
+void sb(t_stack *stack_b)
+{
+    if (stack_b->top == stack_b->size)
+        return ;
+    swap(stack_b);
+    ft_putstr_fd("sb\n", 1);
+}
+
+void ss(t_stack *stack_a, t_stack *stack_b)
+{
+    if (stack_a->top == stack_a->size && stack_b->top == stack_b->size)
+        return ;
+    sa(stack_a);
+    sb(stack_b);
+    ft_putstr_fd("ss\n", 1);
+}
