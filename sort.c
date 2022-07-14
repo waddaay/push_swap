e5305a9a8216aca57bca4b848200f7f7f5461b2b
Yassine Wadday <ywadday@e2r1p5.1337.ma>
2022-07-14 23:34:28 +0100

    100 and 500 works well, need to check norminette and leaks :(

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: ywadday <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 22:54:17 by ywadday           #+#    #+#             */
-/*   Updated: 2022/07/14 05:03:46 by ywadday          ###   ########.fr       */
+/*   Updated: 2022/07/14 23:33:48 by ywadday          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,46 +63,69 @@ int max(t_stack *stack)
     return (max);
 }
 
-void push_all_to_b (t_stack *stack_a, t_stack *stack_b, int div)
+int pick_div(int size, int div)
+{
+    if (size <= 110)
+        div = 3;
+    else if (size <= 190)
+        div = 4;
+    else if (size <= 300)
+        div = 5;
+    else if (size <= 430)
+        div = 6;
+    return (div);
+}
+
+void push_all_to_b (t_stack *stack_a, t_stack *stack_b, int div, int b)
 {
     t_stack *stack_s;
     int     pv1;
     int     pv2 = 0;
-    int     i;
     int     pushed;
     stack_s = sorted_stack(stack_a);
     int vp1 = stack_s->body[0];
-    pv1 = stack_a->size / div;
+    pv1 = (stack_a->size / div);
+    pv2 = pv1 / 2;
     pushed = 0;
     while (stack_b->top > 1)
     {
-        pv2 = (pv1 / 2) + pv2;
-        i = 0;
         while (pushed <= pv1)
         {
             if (stack_b->body[stack_b->top] <= stack_s->body[pv2] && stack_a->body[stack_a->top] > stack_s->body[pv1] && stack_b->top <= stack_b->size - 2 && stack_b->body[stack_b->top] >= vp1)
-                rr(stack_a, stack_b);
+            {
+                rr(stack_a, stack_b, b);
+                stack_a->count++;
+            }
             else if (stack_b->body[stack_b->top] <= stack_s->body[pv2] && stack_b->top <= stack_b->size - 2 && stack_b->body[stack_b->top] >= vp1)
-                    rotate(stack_b, "rb\n");
+            {
+                rotate(stack_b, "rb\n", b);
+                stack_a->count++;
+            }
             if (stack_a->body[stack_a->top] <= stack_s->body[pv1])
             {
-                pb(stack_a, stack_b);
+                pb(stack_a, stack_b, b);
+                stack_a->count++;
                 pushed++;
             }
             else
-                rotate(stack_a, "ra\n");
+            {
+                rotate(stack_a, "ra\n", b);
+                stack_a->count++;
+            }
         }
         vp1 = stack_s->body[pv1];
-        pv1 = ((stack_a->size - pushed) / div) + pushed;
+        pv2 = pv1;
+        div = pick_div(stack_a->size - pushed, div);
+        pv1 = ((stack_a->size - pushed) / div) + pushed + 1;
+        pv2 = ((pv1 - pv2) / 2) + pv2;
     }  
 }
 
-void push_all_to_a(t_stack *stack_a, t_stack *stack_b)
+void push_all_to_a(t_stack *stack_a, t_stack *stack_b, int b)
 {
     int max_indice;
     int max_value;
     int meduim = 0;
-    
     while (stack_a->top > 0)
     {
         meduim = ((stack_b->size - stack_b->top) / 2) + stack_b->top;
@@ -111,13 +134,21 @@ void push_all_to_a(t_stack *stack_a, t_stack *stack_b)
         if (max_indice <= meduim)
         {
             while (stack_b->body[stack_b->top] != max_value)
-                rotate(stack_b, "rb\n");
+            {
+                rotate(stack_b, "rb\n", b);
+                stack_a->count++;
+            }
         }
         else
         {
             while (stack_b->body[stack_b->top] != max_value)
-                rrotate(stack_b, "rrb\n");
+            {
+                rrotate(stack_b, "rrb\n", b);
+                stack_a->count++;
+            }
         }
-        pa(stack_a, stack_b);
+        pa(stack_a, stack_b, b);
+        stack_a->count++;
     }
 }
+
